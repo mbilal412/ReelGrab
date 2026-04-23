3ef58e6579ddaebc10afc269d7f36635e15574c2
Muhammad Bilal <mbilal412004@gmail.com>
2026-04-23 17:58:53 +0500

    solve jsx problem

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
-import ReelProvider from './features/reel/context/reelContext'
+import ReelProvider from './features/reel/context/ReelContext'
 
 
 createRoot(document.getElementById('root')).render(
